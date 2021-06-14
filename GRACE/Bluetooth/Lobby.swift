c3e05908e7c03108c15dc48be54a04b8f686dd2d
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-14 17:59:38 +0800

    Project setup

---
+++ GRACE/Bluetooth/Lobby.swift
@@ -0,0 +1,15 @@
+//
+//  Lobby.swift
+//  GRACE
+//
+//  Created by JiaChen(: on 14/6/21.
+//
+
+import Foundation
+
+struct Lobby {
+    var block: String
+    var currentFloor: Int
+    var lowerboundFloor: Int
+    var upperboundFloor: Int
+}
---
+++ GRACE/Globals/Colors.swift
@@ -0,0 +1,26 @@
+//
+//  Colors.swift
+//  GRACE
+//
+//  Created by JiaChen(: on 14/6/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    /// Custom Almond Color
+    static let almond = UIColor(named: "Almond")!
+    
+    /// Custom Dark Blue Color
+    static let darkBlue = UIColor(named: "DarkBlue")!
+    
+    /// Custom Dark Purple Color
+    static let darkPurple = UIColor(named: "DarkPurple")!
+    
+    /// Custom Green Color
+    static let green = UIColor(named: "Green")!
+    
+    /// Custom Red Color
+    static let red = UIColor(named: "Red")!
+}
