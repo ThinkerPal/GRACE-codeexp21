c9fb7189d7c642b96fdb21fd74a8bf10c7b9fc1b
Thinker Pal <thinkerpal@gmail.com>
2021-06-14 17:30:58 +0800

    Initial Commit

---
+++ GRACE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GRACE
+//
+//  Created by Rui Yang Tan on 14/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
