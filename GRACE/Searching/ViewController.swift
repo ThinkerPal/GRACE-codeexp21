a4048d847c647a0b75b8c20ba20b909880f7a8c4
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-14 18:25:00 +0800

    Created pulsing circle animation

--- GRACE/Searching/ViewController.swift
+++ GRACE/Searching/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var infomationLabel: UILabel!
+    
+    @IBOutlet weak var searchingIndicator: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        startSearching()
+    }
+    
+    func startSearching() {
+        
+        searchingIndicator.transform = .init(scaleX: 0.001, y: 0.001)
+        searchingIndicator.alpha = 0.8
+        
+        UIView.animate(withDuration: 2, delay: 0, options: [ .curveEaseInOut, .repeat]) { [self] in
+            searchingIndicator.transform = .init(scaleX: 1, y: 1)
+            
+            searchingIndicator.alpha = 0
+        }
+    }
 }
 
